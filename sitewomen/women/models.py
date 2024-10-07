f47e1d7f472921aca9e49d441ad463e018aa973d
Ivan <serbakovv79@gmail.com>
2024-10-07 22:04:26 +0400

    Добавил Slug'и для постов
    1) women -> views
    изменил представлние show_post:
    обращение к объекту БД по  get_object_or_404(Women, slug=post_slug)/
    изменил представлние index:
    возврат объекта опубликованных статей
    2) women -> templates
    добавлен новый шаблон post для показа поста
    3) women -> models
    добавлено поле slug/
    выполнена миграция/
    добавил метод в класс Women для reverse возврата  url по slug для вставки в index для ссылки кнопки "Читать пост"

--- sitewomen/women/models.py
+++ sitewomen/women/models.py
@@ -1,11 +1,15 @@
 from django.db import models
+from django.urls import reverse
 
 
 class Women(models.Model):
     title = models.CharField(max_length=255)
+    slug = models.SlugField(max_length=255, blank=True, unique=True, db_index=True)
     content = models.TextField(blank=True)
     time_create = models.DateTimeField(auto_now_add=True)
     time_update = models.DateTimeField(auto_now=True)
     is_published = models.BooleanField(default=True)
 
 # Create your models here.
+    def get_absolute_url(self):
+        return reverse('post', kwargs={'post_slug': self.slug})
