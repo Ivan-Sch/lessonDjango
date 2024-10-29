cc767dbc14a79be781adec569cb1859e915be3d4
Ivan <serbakovv79@gmail.com>
2024-10-29 09:09:17 +0400

    Добавлена панель поиска и панель фильтрации (встроенная и своя)
    1) admin модель регистрации Women
     search_fields = ['title__startswith', 'cat__name'] - чтобы поиск был именно у этих полей (столбцов)
     list_filter = [MarriedFilter, 'cat__name', 'is_published'] - фильтр справа от таблицы, где можно выбирать по какому признаку будут данные в таблице выданы, MarriedFilter - свой собвсвенный фильр, пишется без ковычек
     2) Для собсвенного фильтра создается свой класс class MarriedFilter
     def lookups(self, request, model_admin)- создается для написания ссылки в поискивке /?'status'='married' и обозначения названия параматреа и пункта в фильтре
      def queryset(self, request, queryset создается для свеого отбора, тут пропсиывается вся логика выдачи (какие надо и т.п. с помощью запроса с БД)

--- sitewomen/women/admin.py
+++ sitewomen/women/admin.py
@@ -5,6 +5,23 @@ from .models import Women, Category
 # admin.site.register(Women) #регистрация таблицы (модели)
 
 
+# можно создать свой собсвенный фильтр, со своей собтсвенной логикой
+class MarriedFilter(admin.SimpleListFilter):
+    title = 'Статус женщин'
+    parameter_name = 'status' #создается для написания ссылки в поискивке /?'status'='married'
+
+    def lookups(self, request, model_admin): #создается для написания ссылки в поискивке /?'status'='married'
+        return [
+            ('married', 'Замужем'), #( Названия параметров, Названия пунктов в фильтре)
+            ('single', 'Не замужем'),
+        ]
+
+    def queryset(self, request, queryset): #создается для свеого отбора
+        if self.value() == 'married':
+            return queryset.filter(husband__isnull=False)
+        elif self.value() == 'single':
+            return queryset.filter(husband__isnull=True)
+
 @admin.register(Women)
 class WomenAdmin(admin.ModelAdmin):
     list_display = ('title', 'time_create', 'is_published', 'cat', 'brief_info')
@@ -13,6 +30,10 @@ class WomenAdmin(admin.ModelAdmin):
     ordering = ['-time_create', 'title']
     list_per_page = 5
     actions = ['set_published', "set_draft"] # прописывается для пользовталеского действия
+    search_fields = ['title__startswith', 'cat__name'] # создается поиск по полю т.е. будет писатьсься фраза, часть слова, и будет находится в столбацах указанных
+    list_filter = [MarriedFilter, 'cat__name', 'is_published'] #создается фильтр таблицы справа от таблицы, по каким-то параметрам, которыйе указаны
+    # также тут указана ссылка на класс MarriedFilter- свой собвсвтенный фильтр, со своей логикой работы
+
 
    #создается пользовтаельское поле (стобец) в админ панели, которого нет в БД
     @admin.display(description="Краткое описание", ordering='content' ) #для названия стобца. Если бы его не было, то было бы название метода brief_info. Ordering, чтобы у поля была сортировка
@@ -31,6 +52,8 @@ class WomenAdmin(admin.ModelAdmin):
     def set_draft(self, request, queryset):
         count = queryset.update(is_published=Women.Status.DRAFT)
         self.message_user(request, f"{count} записи(ей) сняты с публикации!", messages.WARNING) #для создания уведомления сверху о действие
+
+
 
 @admin.register(Category)
 class CategoryAdmin(admin.ModelAdmin):
