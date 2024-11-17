c2306d302eef2e78e30f131a85da1c49a9aa5319
Ivan <serbakovv79@gmail.com>
2024-11-17 13:20:08 +0400

    Сделали наследование от UserCreationForm в форме для рег-и и добавли класс пред-я для регистраци
    1) users -> forms
    /// Сделали наследование class RegisterUserForm(UserCreationForm)
    /// даннный класс сам проверяет совпадение паролей и обязательно password1 и 2
    2) users -> views
    /// ЗАменили фун-ю def register на class RegisterUser(CreateView)
    3) И изменили шаблон register.html

--- sitewomen/users/forms.py
+++ sitewomen/users/forms.py
@@ -1,6 +1,6 @@
 from django import forms
 from django.contrib.auth import get_user_model
-from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
 from django.contrib.auth.models import User
 
 
@@ -16,26 +16,32 @@ class LoginUserForm(AuthenticationForm):
         fields = ['username', 'password'] #отображать в форме поля username и password
 
 
-class RegisterUserForm(forms.ModelForm):
-    username = forms.CharField(label="Логин")
-    password = forms.CharField(label="Пароль", widget=forms.PasswordInput)
-    password2 = forms.CharField(label="Повтор пароля", widget=forms.PasswordInput)
+class RegisterUserForm(UserCreationForm):
+    username = forms.CharField(label='Логин', widget=forms.TextInput(attrs={'class': 'form-input'}))
+    password1 = forms.CharField(label='Пароль', widget=forms.PasswordInput(attrs={'class': 'form-input'}))
+    password2 = forms.CharField(label='Повтор пароля', widget=forms.PasswordInput(attrs={'class': 'form-input'}))
 
     class Meta:
         model = get_user_model()
-        fields = ['username', 'email', 'first_name', 'last_name', 'password', 'password2']
+        fields = ['username', 'email', 'first_name', 'last_name', 'password1', 'password2']
         labels = {
             'email': 'E-mail',
             'first_name': 'Имя',
             'last_name': 'Фамилия',
         }
+        widgets = {
+            'email': forms.TextInput(attrs={'class': 'form-input'}),
+            'first_name': forms.TextInput(attrs={'class': 'form-input'}),
+            'last_name': forms.TextInput(attrs={'class': 'form-input'}),
+        }
+
     # ДОП вылидация. ПРОИСХОДИТ ПРИ ПРОВВЕРКИ is_valid() в функ-ии представления,
     # автоматически вызываются при проверке корректности переданных данных
-    def clean_password2(self): #проверка равенства двух паролей
-        cd = self.cleaned_data
-        if cd['password'] != cd['password2']:
-            raise forms.ValidationError("Пароли не совпадают!")
-        return cd['password2']
+    # def clean_password2(self): #проверка равенства двух паролей
+    #     cd = self.cleaned_data
+    #     if cd['password'] != cd['password2']:
+    #         raise forms.ValidationError("Пароли не совпадают!")
+    #     return cd['password2']  УБРАЛИ ПОТОМУ ЧТО ИСПОЛЬЗУЕТСЯ НАСЛДЕОВАНИЕ ОТ UserCreationForm(автоматом проверяет), А если было от forms.ModelForm, то использовали.
 
 
     def clean_email(self): #проверка уникальности email в БД
--- sitewomen/users/urls.py
+++ sitewomen/users/urls.py
@@ -6,5 +6,6 @@ urlpatterns = [
     path('login/', views.LoginUser.as_view(), name='login'),
     path('logout/', LogoutView.as_view(next_page='users:login'), name='logout'), #МОЖНО СРАЗУ ПРОПИСАТЬ обравщение к встоенному классу
     # представления- без добавления своего и наследования от LogoutView
-    path('register/', views.register, name='register'),
+    # path('register/', views.register, name='register'),
+    path('register/', views.RegisterUser.as_view(), name='register')
 ]
