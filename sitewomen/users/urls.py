a8ed1d986cadc9118f88d0d792c7badf2c1a258f
Ivan <serbakovv79@gmail.com>
2024-11-19 21:17:34 +0400

    Добавли изменнеи пароля по Классам PasswordChangeView-явно указали, PasswordChangeDoneView- указали в пути URL навпрямую
    1) users -> forms
    class UserPasswordChangeForm(PasswordChangeForm):
    для переорпдления полей изменения пароля - они обязателны с таким же именем (old_password, new_password1, new_password2) как в PasswordChangeForm. Добавли их чтобы изменить стиль на наш.
    2) users -> views
    Указали для изменения пароля класс пред-я class UserPasswordChange(PasswordChangeView)
    3) users -> URL
    ///Указали URL для изменения пароля
    /// Для перенпаврления после успешного изменения пароля, явно указав обращение к представлению PasswordChangeDoneView и указав в as_view шаблон для отображения этого сайта с напдисью (успешное изменение)
    4) users -> templates
    Добавли шаблоны для изменения пароля и отобржаения надписи с изменнием пароля (password_change_form.html and password_change_done.html)

--- sitewomen/users/urls.py
+++ sitewomen/users/urls.py
@@ -1,4 +1,4 @@
-from django.contrib.auth.views import LogoutView
+from django.contrib.auth.views import LogoutView, PasswordChangeDoneView
 from django.urls import path
 from . import views
 app_name = "users" #т.к. используется namespace="users"
@@ -9,5 +9,10 @@ urlpatterns = [
     # path('register/', views.register, name='register'),
     path('register/', views.RegisterUser.as_view(), name='register'),
     path('profile/', views.ProfileUser.as_view(), name='profile'),
-
+    # Для изменения пароля
+    path('password-change/', views.UserPasswordChange.as_view(), name='password_change'),
+    # Для перенавпрления после успешного изменения
+    path('password-change/done/', PasswordChangeDoneView.as_view(template_name="users/password_change_done.html"), name="password_change_done"),
+    # Для password_change_done мы не создаем форму(не нужна) и не пишем класс предстлавения, т.к. сразу к нему обращаемся,
+    # а чтобы указать свой шаблон, мы пишем в ф-ии as_view
 ]
